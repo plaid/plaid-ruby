9a8556907a49f6a2edd9f6e51bf0ebb48da5f3ca
j4ustin <crites.justin@gmail.com>
2014-02-19 10:03:54 -0600

    Take out bs template files

--- samples/project_samples.rb
+++
@@ -1,9 +0,0 @@
-require 'ruble'
-
-project_sample "Project Sample Example" do |s|
-    s.id = "sample.local"
-    s.category = "com.aptana.projects.samples.web.category"
-    s.location = "samples/local_sample.zip"
-    s.description = "A basic project sample"
-    s.natures = ["com.aptana.projects.webnature"]
-end
--- snippets/snippets.rb
+++
@@ -1,9 +0,0 @@
-require 'ruble'
-
-snippet "Sample Snippet" do |snip|
-  snip.trigger = "sample"
-  snip.expansion = "sample_snippet"
-end
-
-# Use Commands > Bundle Development > Insert Bundle Section > Snippet
-# to easily add new snippets
