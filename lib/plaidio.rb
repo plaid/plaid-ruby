b7b96c771238b3bdf0f891c74e0f32bc02331341
j4ustin <crites.justin@gmail.com>
2014-02-19 13:47:06 -0600

    Tests work, all of the peices are in place to finish this thing off

--- lib/plaidio.rb
+++ lib/plaidio.rb
@@ -1,12 +1,11 @@
+require 'plaidio/config.rb'
 module Plaidio 
   class << self
     include Plaidio::Configure
     
-    
     def self.get 
       return "The getting is good"
     end
-    
     
   end
 end
--- lib/plaidio/config.rb
+++ lib/plaidio/config.rb
@@ -1,6 +1,6 @@
 module Plaidio 
   module Configure 
-    attr_writer[:client_id, :secret]
+    attr_writer :customer_id, :secret
     def config 
       yield self 
       self
