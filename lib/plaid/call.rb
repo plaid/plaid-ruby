07b42f4e76db76e42975cf77abf5fb324dc3593f
Justin Crites <crites.justin@gmail.com>
2014-11-14 11:57:53 -0600

    Set up for merge

--- lib/plaid/call.rb
+++ lib/plaid/call.rb
@@ -9,17 +9,17 @@ module Plaid
         instance_variable_set(:"@#{key}", Plaid.instance_variable_get(:"@#{key}"))
       end
     end
-   
+
     # This is a specific route for auth,
     # it returns specific acct info
     def add_account_auth(type, username, password, email)
       parse_auth_response(post('/auth', type, username, password, email))
     end
-   
+
     # This is a specific route for connect,
     # it returns transaction information
-    def add_account_connect(type,username,password,email)
-      parse_connect_response(post('/connect',type,username,password,email))
+    def add_account_connect(type,username,password,email,options={})
+      parse_connect_response(post('/connect',type,username,password,email,options))
     end
 
     def get_place(id)
@@ -39,7 +39,7 @@ module Plaid
       when 200
         {code: response.code, access_token: parsed['access_token'], accounts: parsed['accounts']}
       when 201
-        {code: response.code, type: parsed['type'], access_token: parsed['access_token'], mfa_info: parsed['mfa_info']}
+        {code: response.code, type: parsed['type'], access_token: parsed['access_token'], mfa: parsed['mfa']}
       else
         {code: response.code, message: parsed}
       end
@@ -50,8 +50,8 @@ module Plaid
       case response.code
       when 200
         {code: response.code, access_token: parsed['access_token'], accounts: parsed['accounts'], transactions: parsed['transactions']}
-      when 201  
-        {code: response.code, type: parsed['type'], access_token: parsed['access_token'], mfa_info: parsed['mfa_info']}
+      when 201
+        {code: response.code, type: parsed['type'], access_token: parsed['access_token'], mfa: parsed['mfa']}
       else
         {code: response.code, message: parsed}
       end
@@ -69,9 +69,9 @@ module Plaid
 
     private
 
-    def post(path,type,username,password,email)
+    def post(path,type,username,password,email,options={})
       url = BASE_URL + path
-      RestClient.post url, client_id: self.instance_variable_get(:'@customer_id') ,secret: self.instance_variable_get(:'@secret'), type: type ,credentials: {username: username, password: password} ,email: email
+      RestClient.post url, client_id: self.instance_variable_get(:'@customer_id') ,secret: self.instance_variable_get(:'@secret'), type: type ,credentials: {username: username, password: password} ,email: email, options: options
     end
 
     def get(path,id = nil)
