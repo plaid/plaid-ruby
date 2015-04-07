5c514f04312f784d71da3cedbd83e8db66b8fa83
Ho-Sheng Hsiao <hosh@getwherewithal.com>
2015-04-06 19:32:34 -0700

    Refactored auth
      - Refactored specs (see http://betterspecs.org/#let) to use rspec
        idioms
      - Fixed inconsistency between add_spec and user_spec
      - Use built-in JSON library to generate a JSON string from the Hash.
        While the maintainer has asked to not have any runtime dependencies,
        using JSON can be problematic depending on Ruby version or if the
        gem is being used inside Rails or other projects. This needs more
        discussion and refinement.

--- lib/plaid/add_user.rb
+++ lib/plaid/add_user.rb
@@ -1,24 +1,26 @@
+require 'json'
+
 module Plaid
   module AddUser
-      def add_user(api_level,username,password,type,pin=nil,options=nil)
-        begin
-          payload = {username:username,password:password,type:type}
-          payload.merge!(pin:pin) if pin
-          payload.merge!(options: options) if options
-          res = self.post(api_level,payload)
-          self.user(res,api_level)
-        rescue => e
-          error_handler(e)
-        end
-      end
+    # Required parameters:
+    #   api_level, username, password, type
+    def add_user(api_level, username, password, type, pin = nil, options = nil)
+      payload = { username: username, password: password, type: type }
 
-    def set_user(token,api_level=[],type=nil)
+      payload[:pin]     = pin if pin
+      payload[:options] = options.is_a?(Hash) ? JSON.generate(options) : options if options
+
+      res = self.post(api_level, payload)
+      self.user(res, api_level)
+    rescue => e
+      error_handler e
+    end
+
+    def set_user(token, api_level=[], type=nil)
       token = token + '_' + type unless type.nil?
-      begin
-        self.existing_user(token,api_level)
-      rescue => e
-        error_handler(e)
-      end
+      self.existing_user(token,api_level)
+    rescue => e
+      error_handler e
     end
   end
 end
