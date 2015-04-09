f40bd6a077865183f086b4e1e9e07b7010798688
Ho-Sheng Hsiao <hosh@getwherewithal.com>
2015-04-09 11:58:46 -0700

    Refactored Plaid::Institution to use semi-private build API

--- lib/plaid/models/institution.rb
+++ lib/plaid/models/institution.rb
@@ -4,16 +4,23 @@ module Plaid
     attr_accessor :id, :name, :type, :has_mfa, :mfa
 
     class << self
+      # API: semi-private
+      # This method takes an array returned from the API and instantiates
+      # all of the categories
       def all(res)
-        res.map { |cat| self.one(cat) }
+        res.map { |cat| self.build(cat) }
       end
 
-      def one(res)
-        self.new.build(res)
+      # API: semi-private
+      # This method instantiates and builds a single category
+      def build(res)
+        self.new.update(res)
       end
     end
 
-    def build(inst)
+    # API: semi-private
+    # This method updates a Plaid:Institution based upon the results from the API
+    def update(inst)
       self.id = inst['id']
       self.name = inst['name']
       self.type = inst['type']
