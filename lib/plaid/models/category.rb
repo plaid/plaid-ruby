15b3547ff5c7406cd132ddbaccd564fa22946580
Mitch Lee <mitchlee11@gmail.com>
2015-04-28 14:20:30 -0700

    Fix category parsing.

--- lib/plaid/models/category.rb
+++ lib/plaid/models/category.rb
@@ -2,28 +2,22 @@ module Plaid
   class Category
     attr_accessor :type, :hierarchy, :id
 
-    class << self
-      # API: semi-private
-      # This method takes an array returned from the API and instantiates
-      # all of the categories
-      def all(res)
-        res.map { |cat| self.build(cat) }
-      end
-
-      # API: semi-private
-      # This method instantiates and builds a single category
-      def build(res)
-        self.new.update(res)
-      end
+    def initialize(type, hierarchy, id)
+      @type = type
+      @hierarchy = hierarchy
+      @id = id
     end
 
     # API: semi-private
-    # This method updates a Plaid:Category based upon the results from the API
-    def update(cat)
-      self.type = cat['type']
-      self.hierarchy = cat['hierarchy']
-      self.id = cat['id']
-      return self
+    # This method takes an array returned from the API and instantiates all of the categories
+    def self.all(res)
+      res.map { |cat| build(cat) }
+    end
+
+    # API: semi-private
+    # This method instantiates and builds a single category
+    def self.build(cat)
+      new(cat['type'], cat['hierarchy'], cat['id'])
     end
   end
 end
