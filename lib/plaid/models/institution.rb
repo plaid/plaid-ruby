4849b8cabf1a58fef23f8c6fbcb75d44cb4af686
Mitch Lee <mitchlee11@gmail.com>
2015-04-28 14:30:02 -0700

    Fix institution parsing.

--- lib/plaid/models/institution.rb
+++ lib/plaid/models/institution.rb
@@ -1,33 +1,26 @@
 module Plaid
   class Institution
-
     attr_accessor :id, :name, :type, :has_mfa, :mfa
 
-    class << self
-      # API: semi-private
-      # This method takes an array returned from the API and instantiates
-      # all of the categories
-      def all(res)
-        res.map { |cat| self.build(cat) }
-      end
-
-      # API: semi-private
-      # This method instantiates and builds a single category
-      def build(res)
-        self.new.update(res)
-      end
+    def initialize(id, name, type, has_mfa, mfa)
+      @id = id
+      @name = name
+      @type = type
+      @has_mfa = has_mfa
+      @mfa = mfa
     end
 
     # API: semi-private
-    # This method updates a Plaid:Institution based upon the results from the API
-    def update(inst)
-      self.id = inst['id']
-      self.name = inst['name']
-      self.type = inst['type']
-      self.has_mfa = inst['has_mfa']
-      self.mfa = inst['mfa']
-      return self
+    # This method takes an array returned from the API and instantiates all of the institutions
+    def self.all(res)
+      res.map { |inst| build(inst) }
     end
 
+    # API: semi-private
+    # This method instantiates and builds a single institutions
+    def self.build(inst)
+      new(inst['id'], inst['name'], inst['type'], inst['has_mfa'], inst['mfa'])
+    end
   end
 end
+
