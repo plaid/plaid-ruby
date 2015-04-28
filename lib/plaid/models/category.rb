e98d304d18ac5b2701012c1693fb316660413909
Mitch Lee <mitchlee11@gmail.com>
2015-04-28 16:30:51 -0700

    Replace build class methods on Category and Institution with instantiation from hashes.

--- lib/plaid/models/category.rb
+++ lib/plaid/models/category.rb
@@ -2,22 +2,16 @@ module Plaid
   class Category
     attr_accessor :type, :hierarchy, :id
 
-    def initialize(type, hierarchy, id)
-      @type = type
-      @hierarchy = hierarchy
-      @id = id
+    def initialize(fields = {})
+      @type = fields['type']
+      @hierarchy = fields['hierarchy']
+      @id = fields['id']
     end
 
     # API: semi-private
     # This method takes an array returned from the API and instantiates all of the categories
     def self.all(res)
-      res.map { |cat| build(cat) }
-    end
-
-    # API: semi-private
-    # This method instantiates and builds a single category
-    def self.build(cat)
-      new(cat['type'], cat['hierarchy'], cat['id'])
+      res.map { |cat| new(cat) }
     end
   end
 end
--- lib/plaid/models/institution.rb
+++ lib/plaid/models/institution.rb
@@ -2,24 +2,18 @@ module Plaid
   class Institution
     attr_accessor :id, :name, :type, :has_mfa, :mfa
 
-    def initialize(id, name, type, has_mfa, mfa)
-      @id = id
-      @name = name
-      @type = type
-      @has_mfa = has_mfa
-      @mfa = mfa
+    def initialize(fields = {})
+      @id = fields['id']
+      @name = fields['name']
+      @type = fields['type']
+      @has_mfa = fields['has_mfa']
+      @mfa = fields['mfa']
     end
 
     # API: semi-private
     # This method takes an array returned from the API and instantiates all of the institutions
     def self.all(res)
-      res.map { |inst| build(inst) }
-    end
-
-    # API: semi-private
-    # This method instantiates and builds a single institutions
-    def self.build(inst)
-      new(inst['id'], inst['name'], inst['type'], inst['has_mfa'], inst['mfa'])
+      res.map { |inst| new(inst) }
     end
   end
 end
