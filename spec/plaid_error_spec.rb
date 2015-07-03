368877e95dd6247097a8108f1ba317e64609557a
John Bunting <codingjester@gmail.com>
2015-07-03 11:27:20 -0400

    Adding specs for connection and PlaidError

---
+++ spec/plaid_error_spec.rb
@@ -0,0 +1,12 @@
+require 'spec_helper'
+
+RSpec.describe Plaid::PlaidError do
+  describe "#new" do
+    it "should allow code, message and resolution" do
+      error = Plaid::PlaidError.new 1, "testing", "fix it"
+      expect(error.code).to eq(1)
+      expect(error.message).to eq("testing")
+      expect(error.resolve).to eq("fix it")
+    end
+  end
+end
