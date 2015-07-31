33862f8bbd7223726f855ac3d35b4d4e42054207
Dana Merrick <dana@ifttt.com>
2015-07-31 13:49:26 -0700

    Forcing use_ssl to be true.

--- lib/plaid/connection.rb
+++ lib/plaid/connection.rb
@@ -10,7 +10,7 @@ module Plaid
         uri = build_uri(path)
         options.merge!(client_id: Plaid.customer_id, secret: Plaid.secret)
         http = Net::HTTP.new(uri.host, uri.port)
-        http.use_ssl = (uri.scheme == 'https')
+        http.use_ssl = true
         request = Net::HTTP::Post.new(uri.path)
         request.set_form_data(options)
         res = http.request(request)
@@ -21,7 +21,7 @@ module Plaid
       def get(path, id = nil)
         uri = build_uri(path,id)
         http = Net::HTTP.new(uri.host, uri.port)
-        http.use_ssl = (uri.scheme == 'https')
+        http.use_ssl = true
         request = Net::HTTP::Get.new(uri.path)
         res = http.request(request)
         parse_get_response(res.body)
@@ -34,7 +34,7 @@ module Plaid
         req = Net::HTTP::Get.new(uri.path)
         req.body = URI.encode_www_form(options) if options
         req.content_type = 'application/x-www-form-urlencoded'
-        res = Net::HTTP.start(uri.hostname, uri.port, :use_ssl => uri.scheme == 'https') { |http| http.request(req) }
+        res = Net::HTTP.start(uri.hostname, uri.port, use_ssl: true) { |http| http.request(req) }
         parse_response(res)
       end
 
@@ -45,7 +45,7 @@ module Plaid
         req = Net::HTTP::Patch.new(uri.path)
         req.body = URI.encode_www_form(options) if options
         req.content_type = 'application/x-www-form-urlencoded'
-        res = Net::HTTP.start(uri.hostname, uri.port, :use_ssl => uri.scheme == 'https') { |http| http.request(req) }
+        res = Net::HTTP.start(uri.hostname, uri.port, use_ssl: true) { |http| http.request(req) }
         parse_response(res)
       end
 
@@ -56,7 +56,7 @@ module Plaid
         req = Net::HTTP::Delete.new(uri.path)
         req.body = URI.encode_www_form(options) if options
         req.content_type = 'application/x-www-form-urlencoded'
-        Net::HTTP.start(uri.hostname, uri.port, :use_ssl => uri.scheme == 'https') { |http| http.request(req) }
+        Net::HTTP.start(uri.hostname, uri.port, use_ssl: true) { |http| http.request(req) }
       end
 
       protected
