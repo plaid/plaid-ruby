61aff56e630dc37a93b435274f4a2e6dadc8190f
Dana Merrick <dana@plaid.com>
2015-12-16 13:07:12 -0800

    Including stripe_bank_account_token, if available.

--- lib/plaid/models/exchange_token_response.rb
+++ lib/plaid/models/exchange_token_response.rb
@@ -1,9 +1,11 @@
 module Plaid
   class ExchangeTokenResponse
     attr_accessor :access_token
+    attr_accessor :stripe_bank_account_token
 
     def initialize(fields = {})
       @access_token = fields['access_token']
+      @stripe_bank_account_token = fields['stripe_bank_account_token']
     end
   end
 end
