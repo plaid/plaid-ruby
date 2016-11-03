cbe8acfdaec6427d90e9ee1384993a39b766b718
Oleg Dashevskii <odashevskii@plaid.com>
2016-11-03 20:41:25 +0700

    Add income and risk webhook support

--- test/test_webhook.rb
+++ test/test_webhook.rb
@@ -17,6 +17,8 @@ class PlaidWebhookTest < MiniTest::Test
     refute_predicate webhook, :removed_transaction?
     refute_predicate webhook, :user_webhook_updated?
     refute_predicate webhook, :error_response?
+    refute_predicate webhook, :income?
+    refute_predicate webhook, :risk?
   end
 
   def test_removed_transaction_webhook
@@ -34,8 +36,28 @@ class PlaidWebhookTest < MiniTest::Test
     assert_predicate webhook, :removed_transaction?
     refute_predicate webhook, :user_webhook_updated?
     refute_predicate webhook, :error_response?
+    refute_predicate webhook, :income?
+    refute_predicate webhook, :risk?
 
     assert_equal %w(1 2 3), webhook.removed_transactions_ids
+  end
+
+  def test_income
+    webhook = Plaid::Webhook.new(
+      'message' => 'income data available',
+      'access_token' => '123456',
+      'code' => 10)
+
+    assert_equal 'INCOME', webhook.type
+
+    refute_predicate webhook, :initial_transaction?
+    refute_predicate webhook, :historical_transaction?
+    refute_predicate webhook, :normal_transaction?
+    refute_predicate webhook, :removed_transaction?
+    refute_predicate webhook, :user_webhook_updated?
+    refute_predicate webhook, :error_response?
+    assert_predicate webhook, :income?
+    refute_predicate webhook, :risk?
   end
 
   def test_error
@@ -54,11 +76,13 @@ class PlaidWebhookTest < MiniTest::Test
     refute_predicate webhook, :removed_transaction?
     refute_predicate webhook, :user_webhook_updated?
     assert_predicate webhook, :error_response?
+    refute_predicate webhook, :income?
+    refute_predicate webhook, :risk?
 
     err = webhook.error
     assert_kind_of Plaid::PlaidError, err
     assert_equal 1205, err.code
-    assert_equal 'Code 1205: account locked. The account is locked. ' + 
+    assert_equal 'Code 1205: account locked. The account is locked. ' +
                  "Please prompt the user to visit the issuing institution's " +
                  'site and unlock their account.', err.message
   end
