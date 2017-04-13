0240c04307bc0fe7e689e3434f445555d55600aa
Jason Charnes <jason@thecharnes.com>
2017-04-12 19:50:54 -0700

    Adds options hash for credit_details (#169)

--- lib/plaid/products/credit_details.rb
+++ lib/plaid/products/credit_details.rb
@@ -11,10 +11,17 @@ module Plaid
     # and access_token's item
     #
     # access_token - access_token who's item to fetch credit_details for
+    # account_ids  - Specific account ids to fetch credit_details for (optional)
     #
     # Returns a parsed JSON of credit_details information
-    def get(access_token)
-      payload = { access_token: access_token }
+    def get(access_token, account_ids: nil)
+      options_payload = {}
+      options_payload[:account_ids] = account_ids unless account_ids.nil?
+      payload = {
+        access_token: access_token,
+        options: options_payload
+      }
+
       @client.post_with_auth('credit_details/get', payload)
     end
   end
--- test/test_credit_details.rb
+++ test/test_credit_details.rb
@@ -18,11 +18,24 @@ class PlaidCreditDetailsTest < PlaidTest
     response = @client.credit_details.get(@access_token)
     refute_empty(response['accounts'])
     refute_empty(response['credit_details'])
+
+    account_id = response['accounts'][3]['account_id']
+    response = @client.credit_details.get(
+      @access_token,
+      account_ids: [account_id]
+    )
+    assert_equal(1, response['credit_details'].length)
   end
 
   def test_get_invalid_access_token
     assert_raises(Plaid::InvalidInputError) do
       @client.credit_details.get(BAD_STRING)
+    end
+  end
+
+  def test_get_invalid_options
+    assert_raises(Plaid::InvalidRequestError) do
+      @client.credit_details.get(@access_token, account_ids: BAD_STRING)
     end
   end
 end
