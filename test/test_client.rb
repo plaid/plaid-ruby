3f42e28d9c0e232096bc4f8ba0b7796d6542cb5e
Oleg Dashevskii <odashevskii@plaid.com>
2017-07-12 19:31:25 +0700

    test_client

--- test/test_client.rb
+++ test/test_client.rb
@@ -1,25 +1,27 @@
-require_relative 'plaid_test'
+require_relative 'test_helper'
 
 # Internal: The test for Plaid::Client.
 class PlaidClientTest < PlaidTest
-  def test_env_setting
-    client = Plaid::Client.new(env: :sandbox,
-                               client_id: ENV['PLAID_RUBY_CLIENT_ID'],
-                               secret: ENV['PLAID_RUBY_SECRET'],
-                               public_key: ENV['PLAID_RUBY_PUBLIC_KEY'])
-    assert_equal(Plaid.url_from_env(:sandbox), client.instance_variable_get(:@env))
+  unless STUB_API
+    def test_env_setting
+      client = Plaid::Client.new(env: :sandbox,
+                                 client_id: ENV['PLAID_RUBY_CLIENT_ID'],
+                                 secret: ENV['PLAID_RUBY_SECRET'],
+                                 public_key: ENV['PLAID_RUBY_PUBLIC_KEY'])
+      assert_equal(Plaid.url_from_env(:sandbox), client.instance_variable_get(:@env))
 
-    client = Plaid::Client.new(env: :development,
-                               client_id: ENV['PLAID_RUBY_CLIENT_ID'],
-                               secret: ENV['PLAID_RUBY_SECRET'],
-                               public_key: ENV['PLAID_RUBY_PUBLIC_KEY'])
-    assert_equal(Plaid.url_from_env(:development), client.instance_variable_get(:@env))
+      client = Plaid::Client.new(env: :development,
+                                 client_id: ENV['PLAID_RUBY_CLIENT_ID'],
+                                 secret: ENV['PLAID_RUBY_SECRET'],
+                                 public_key: ENV['PLAID_RUBY_PUBLIC_KEY'])
+      assert_equal(Plaid.url_from_env(:development), client.instance_variable_get(:@env))
 
-    assert_raises(ArgumentError) do
-      Plaid::Client.new(env: :BAD_ENV,
-                        client_id: ENV['PLAID_RUBY_CLIENT_ID'],
-                        secret: ENV['PLAID_RUBY_SECRET'],
-                        public_key: ENV['PLAID_RUBY_PUBLIC_KEY'])
+      assert_raises(ArgumentError) do
+        Plaid::Client.new(env: :BAD_ENV,
+                          client_id: ENV['PLAID_RUBY_CLIENT_ID'],
+                          secret: ENV['PLAID_RUBY_SECRET'],
+                          public_key: ENV['PLAID_RUBY_PUBLIC_KEY'])
+      end
     end
   end
 end
