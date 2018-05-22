1718a01a28a5464583de9fa7ceac3ed677e95b81
Rohan Shah <rshah@plaid.com>
2018-05-21 22:56:49 -0700

    fix auth and version header issues

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -26,7 +26,6 @@ class PlaidTest < MiniTest::Test
     client_id = ENV['PLAID_RUBY_CLIENT_ID']
     secret = ENV['PLAID_RUBY_SECRET']
     public_key = ENV['PLAID_RUBY_PUBLIC_KEY']
-
     if STUB_API && RECORD_MODE == :none
       # In non-recording mode we use stubs for auth
       # (see also task :vcr_hide_credentials in Rakefile)
