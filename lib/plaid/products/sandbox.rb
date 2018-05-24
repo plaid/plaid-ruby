c870b7344c2facb9e046e9160582536fdaefe641
Evan Putra Limanto <evanlimanto@berkeley.edu>
2018-05-24 08:04:53 -0700

    add sandbox-only endpoint (#195)

--- lib/plaid/products/sandbox.rb
+++ lib/plaid/products/sandbox.rb
@@ -23,11 +23,60 @@ module Plaid
     end
   end
 
+  # Public: Class used to call the SandboxPublicToken sub-product
+  class SandboxPublicToken < BaseProduct
+    # Public: Creates a public token for sandbox testing.
+    #
+    # Does a POST /sandbox/public_token/create call which can be used
+    # to generate a public_token given an institution and list of
+    # products.
+    #
+    # institution_id   - Specific institution id to generate token for.
+    # initial_products - Products for which generated token is valid for.
+    # webhook          - webhook to associate with the item (optional).
+    # options          - Additional options to merge into API request.
+    #
+    # Returns a SandboxCreateResponse object with a public token and item id.
+    def create(institution_id:,
+               initial_products:,
+               webhook: nil,
+               options: nil)
+
+      options_payload = {}
+      options_payload[:webhook] = webhook unless webhook.nil?
+      options_payload = options_payload.merge(options) unless options.nil?
+
+      post_with_public_key 'sandbox/public_token/create',
+                           SandboxCreateResponse,
+                           institution_id: institution_id,
+                           initial_products: initial_products,
+                           options: options_payload
+    end
+
+    # Public: Response for /sandbox/public_token/create.
+    class SandboxCreateResponse < Models::BaseResponse
+      ##
+      # :attr_reader:
+      # Public: The String token.
+      property :public_token
+
+      ##
+      # :attr_reader:
+      # Public: The String item ID.
+      property :item_id
+    end
+  end
+
   # Public: Class used to call the Sandbox product.
   class Sandbox < BaseProduct
     ##
     # :attr_reader:
     # Public: The Plaid::SandboxItem product accessor.
     subproduct :sandbox_item
+
+    ##
+    # :attr_reader:
+    # Public: The Plaid::SandboxPublicToken product accessor.
+    subproduct :sandbox_public_token
   end
 end
