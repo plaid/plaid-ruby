b6262dbd0eebaf8bf480692ce2635e6d041c7794
David Fish <dfish@plaid.com>
2018-06-19 10:23:19 -0700

    stuff works

--- lib/plaid/products/asset_report.rb
+++ lib/plaid/products/asset_report.rb
@@ -1,19 +1,20 @@
 module Plaid
-  # Public: Class used to call the Assets product.
+  # Public: Class used to call the Assets product. Documentation available at
+  # https://plaid.com/docs/api/#assets.
   class AssetReport < BaseProduct
-    # Public: Get information about all available balances.
+    # Public: Create an asset report.
     #
-    # Does a POST /accounts/balance/get call to get real-time balance
-    # information for all accounts associated with the access_token's item.
+    # access_tokens  - An array of access tokens, one token for each Item to be
+    #                  included in the Asset Report.
+    # days_requested - Days of transaction history requested to be included in
+    #                  the Asset Report. Plaid will return as much data as
+    #                  possible within the implied date range, up to a maximum
+    #                  of 730 days (2 years) of history.
+    # options -        An optional object containing client-provided user
+    #                  information, a client-provided report ID, and a webhook.
+    #                  For more information, see the website listed above.
     #
-    # access_token - The String access_token for the item to fetch balances
-    #                for.
-    # account_ids  - The Array with specific account IDs to fetch balances for
-    #                (optional).
-    # options      - Additional options to be merged into the request
-    #                (optional).
-    #
-    # Returns the AccountsResponse object with accounts information.
+    # Returns a AssetReportCreateResponse object.
     def create(access_tokens, days_requested, options)
       post_with_auth 'asset_report/create',
                     AssetReportCreateResponse,
@@ -24,11 +25,69 @@ module Plaid
                     }
     end
 
+    # Public: Retrieve an asset report.
+    #
+    # asset_report_token - The asset report token from the `create` response.
+    #
+    # Returns a AssetReportGetResponse object.
     def get(asset_report_token)
       post_with_auth 'asset_report/get',
                     AssetReportGetResponse,
                     {
                       asset_report_token: asset_report_token,
+                    }
+    end
+
+    # Public: Retrieve an asset report as a PDF.
+    #
+    # asset_report_token - The asset report token from the `create` response.
+    #
+    # Returns the PDF.
+    def get_pdf(asset_report_token)
+      client.post_with_auth('asset_report/pdf/get', {
+        asset_report_token: asset_report_token,
+      })
+    end
+
+    # Public: Remove an asset report.
+    #
+    # asset_report_token - The asset report token from the `create` response.
+    #
+    # Returns a AssetReportRemoveResponse object.
+    def remove(asset_report_token)
+      post_with_auth 'asset_report/remove',
+                    AssetReportRemoveResponse,
+                    {
+                      asset_report_token: asset_report_token,
+                    }
+    end
+
+    # Public: Create an audit copy token.
+    #
+    # asset_report_token - The asset report token from the `create` response.
+    # auditor_id         - The ID of the third party with which you would like
+    #                      to share the asset report.
+    #
+    # Returns a AuditCopyCreateResponse object.
+    def create_audit_copy(asset_report_token, auditor_id)
+      post_with_auth 'asset_report/audit_copy/create',
+                    AuditCopyCreateResponse,
+                    {
+                      asset_report_token: asset_report_token,
+                      auditor_id: auditor_id,
+                    }
+    end
+
+    # Public: Remove an audit copy token.
+    #
+    # audit_copy_token - The audit copy token from the `create` response.
+    #
+    # Returns a AuditCopyRemoveResponse object.
+    def remove_audit_copy(audit_copy_token)
+      post_with_auth 'asset_report/audit_copy/remove',
+                    AuditCopyRemoveResponse,
+                    {
+                      audit_copy_token: audit_copy_token,
                     }
     end
   end
@@ -37,12 +96,12 @@ module Plaid
   class AssetReportCreateResponse < Models::BaseResponse
     ##
     # :attr_reader:
-    # Public: The asset report token: String.
+    # Public: The asset report token.
     property :asset_report_token
 
     ##
     # :attr_reader:
-    # Public: The asset report ID: String.
+    # Public: The asset report ID.
     property :asset_report_id
   end
 
@@ -50,12 +109,36 @@ module Plaid
   class AssetReportGetResponse < Models::BaseResponse
     ##
     # :attr_reader:
-    # Public: The asset report token: String.
+    # Public: The asset report token.
     property :report, coerce: Models::AssetReport
 
     ##
     # :attr_reader:
     # Public: Warnings encountered during asset report generation.
     property :warnings, coerce: Array[Models::Warning]
+  end
+
+  # Public: The response wrapper for /asset_report/remove.
+  class AssetReportRemoveResponse < Models::BaseResponse
+    ##
+    # :attr_reader:
+    # Public: A boolean indicating whether the report was removed.
+    property :removed
+  end
+
+  # Public: The response wrapper for /audit_copy/create.
+  class AuditCopyCreateResponse < Models::BaseResponse
+    ##
+    # :attr_reader:
+    # Public: The audit copy token.
+    property :audit_copy_token
+  end
+
+  # Public: The response wrapper for /audit_copy/remove.
+  class AuditCopyRemoveResponse < Models::BaseResponse
+    ##
+    # :attr_reader:
+    # Public: A boolean indicating whether the audit copy was removed.
+    property :removed
   end
 end
--- test/test_asset_report.rb
+++ test/test_asset_report.rb
@@ -1,13 +1,15 @@
 require_relative 'test_helper'
 
 # Internal: The test for Plaid::AssetReport.
-class PlaidAssetsTest < PlaidTest
+class PlaidAssetReportTest < PlaidTest
 
   # rubocop:disable Metrics/AbcSize
 
   def test_full_flow
+    # Create an item with the "assets" product.
     create_item initial_products: [:assets]
 
+    # Create an asset report.
     days_requested = 60
     options = {
       client_report_id: '123',
@@ -22,13 +24,12 @@ class PlaidAssetsTest < PlaidTest
         email: 'jane.doe@example.com',
       },
     }
-
     response = @client.asset_report.create([access_token], days_requested, options)
     refute_empty(response.asset_report_id)
     refute_empty(response.asset_report_token)
 
+    # Poll until report generation has finished.
     asset_report_token = response.asset_report_token
-
     retries = 20
     while
       begin
@@ -46,9 +47,23 @@ class PlaidAssetsTest < PlaidTest
         end
       end
     end
-
     refute_empty(response.report)
 
+    # Get the asset report as a PDF.
+    pdf = @client.asset_report.get_pdf(asset_report_token)
+    refute_empty(pdf)
 
+    # Create an audit copy token.
+    response = @client.asset_report.create_audit_copy(
+      asset_report_token, 'fannie_mae')
+    refute_empty(response.audit_copy_token)
+
+    # Remove the audit copy.
+    response = @client.asset_report.remove_audit_copy(response.audit_copy_token)
+    assert_equal(response.removed, true)
+
+    # Remove the asset report.
+    response = @client.asset_report.remove(asset_report_token)
+    assert_equal(response.removed, true)
   end
 end
