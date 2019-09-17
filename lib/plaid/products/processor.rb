0b17c7280604c3974692f2941c1dba7cef975233
Jeffrey "Jake" Barszcz <jbarszcz@plaid.com>
2019-09-17 13:01:09 -0400

    Add support for Ocrolus as a processor (#237)
    * Add Ocrolus processor
    
    * lint

--- lib/plaid/products/processor.rb
+++ lib/plaid/products/processor.rb
@@ -98,6 +98,34 @@ module Plaid
     subproduct :processor_token, ProcessorToken
   end
 
+  # Public: Class used to call the Ocrolus sub-product.
+  class Ocrolus < BaseProduct
+    # Public: Class used to call the ocrolus.processor_token subproduct
+    class ProcessorToken < BaseProduct
+      # Public: Creates a Ocrolus processor token from an access_token.
+      #
+      # Does a POST /processor/ocrolus/processor_token/create call which can be
+      # used to generate a Ocrolus processor token for a given account ID.
+      #
+      # access_token - access_token to create a public token for.
+      # account_id - ID of the account to create a processor token for.
+      #
+      # Returns a ProcessorTokenResponse object containing a Ocrolus processor
+      # token.
+      def create(access_token, account_id)
+        post_with_auth 'processor/ocrolus/processor_token/create',
+                       ProcessorTokenResponse,
+                       access_token: access_token,
+                       account_id: account_id
+      end
+    end
+
+    ##
+    # :attr_reader:
+    # Public: The Plaid::Ocrolus::ProcessorToken product accessor.
+    subproduct :processor_token, ProcessorToken
+  end
+
   # Public: Class used to call the Processor product.
   class Processor < BaseProduct
     ##
@@ -114,5 +142,10 @@ module Plaid
     # :attr_reader:
     # Public: The Plaid::Apex product accessor.
     subproduct :apex
+
+    ##
+    # :attr_reader:
+    # Public: The Plaid::Ocrolus product accessor.
+    subproduct :ocrolus
   end
 end
