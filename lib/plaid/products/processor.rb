0542bdbcaf44669760355f87b9b2ef99d51365cb
Julien Chien <julien@moderntreasury.com>
2019-12-04 19:10:10 -0800

    processor

--- lib/plaid/products/processor.rb
+++ lib/plaid/products/processor.rb
@@ -126,6 +126,34 @@ module Plaid
     subproduct :processor_token, ProcessorToken
   end
 
+  # Public: Class used to call the Modern Treasury sub-product.
+  class ModernTreasury < BaseProduct
+    # Public: Class used to call the modern_treasury.processor_token subproduct
+    class ProcessorToken < BaseProduct
+      # Public: Creates a Modern Treasury processor token from an access_token.
+      #
+      # Does a POST /processor/modern_treasury/processor_token/create call which can be
+      # used to generate a Modern Treasury processor token for a given account ID.
+      #
+      # access_token - access_token to create a public token for.
+      # account_id - ID of the account to create a processor token for.
+      #
+      # Returns a ProcessorTokenResponse object containing a Ocrolus processor
+      # token.
+      def create(access_token, account_id)
+        post_with_auth 'processor/modern_treasury/processor_token/create',
+                       ProcessorTokenResponse,
+                       access_token: access_token,
+                       account_id: account_id
+      end
+    end
+
+    ##
+    # :attr_reader:
+    # Public: The Plaid::ModernTreasury::ProcessorToken product accessor.
+    subproduct :processor_token, ProcessorToken
+  end
+
   # Public: Class used to call the Processor product.
   class Processor < BaseProduct
     ##
@@ -147,5 +175,10 @@ module Plaid
     # :attr_reader:
     # Public: The Plaid::Ocrolus product accessor.
     subproduct :ocrolus
+
+    ##
+    # :attr_reader:
+    # Public: The Plaid::ModernTreasury product accessor.
+    subproduct :modern_treasury
   end
 end
