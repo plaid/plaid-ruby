c5078e32024c59c2bc1f63322b0576edecbe5248
Andrew Chen <andrewchen@plaid.com>
2020-07-08 16:38:55 -0700

    use a list of field names

--- lib/plaid/products/link_token.rb
+++ lib/plaid/products/link_token.rb
@@ -1,24 +1,32 @@
+$link_token_field_names = [
+   :user,
+   :client_name,
+   :products,
+   :country_codes,
+   :language,
+   :webhook,
+   :access_token,
+   :link_customization_name,
+   :redirect_uri,
+   :android_package_name,
+   :account_filters,
+   :cross_app_item_add,
+   :payment_initiation
+]
+
 module Plaid
   # Public: Class used to call the LinkToken sub-product.
   class LinkToken < BaseProduct
     def create(configs)
+      body = {}
+
+      for field in $link_token_field_names do
+        body[field] = configs[field]
+      end
+
       post_with_auth 'link/token/create',
                      CreateResponse,
-                     user: configs[:user],
-                     client_name: configs[:client_name],
-                     products: configs[:products],
-                     country_codes: configs[:country_codes],
-                     language: configs[:language],
-                     locale: configs[:locale],
-                     webhook: configs[:webhook],
-                     access_token: configs[:access_token],
-                     link_customization_name: configs[:link_configuration_name],
-                     redirect_uri: configs[:redirect_uri],
-                     android_package_name: configs[:android_package_name],
-                     institution_id: configs[:institution_id],
-                     account_filters: configs[:account_filters],
-                     cross_app_item_add: configs[:cross_app_item_add],
-                     payment_initiation: configs[:payment_initiation]
+                     body
     end
   end
 
@@ -27,7 +35,7 @@ module Plaid
     ##
     # :attr_reader:
     # Public: The String token.
-    property :token
+    property :link_token
 
     ##
     # :attr_reader:
