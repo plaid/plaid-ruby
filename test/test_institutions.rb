4c1d5803c72fbb7ab2a49b901eb8f57fa1c82697
jasonjia93 <jasonjia93@users.noreply.github.com>
2020-10-01 16:07:22 -0700

    Add country codes to institutions/ routes (#306)

--- test/test_institutions.rb
+++ test/test_institutions.rb
@@ -3,13 +3,16 @@ require_relative 'test_helper'
 # Internal: The test for Plaid::Institutions.
 class PlaidInstitutionsTest < PlaidTest
   def test_get
-    response = client.institutions.get(count: 3, offset: 1)
+    response = client.institutions.get(count: 3,
+                                       offset: 1,
+                                       country_codes: ['US'])
     assert_equal(3, response.institutions.length)
   end
 
   def test_get_with_options
     response = client.institutions.get count: 3,
                                        offset: 1,
+                                       country_codes: ['US'],
                                        options: { products: ['transactions'],
                                                   include_optional_metadata:
                                                   true }
@@ -18,24 +21,27 @@ class PlaidInstitutionsTest < PlaidTest
 
   def test_get_invalid_parameters
     assert_raises(Plaid::InvalidRequestError) do
-      client.institutions.get(count: BAD_STRING, offset: BAD_STRING)
+      client.institutions.get(count: BAD_STRING,
+                              offset: BAD_STRING,
+                              country_codes: ['US'])
     end
   end
 
   def test_get_by_id
-    response = client.institutions.get_by_id(SANDBOX_INSTITUTION)
+    response = client.institutions.get_by_id(SANDBOX_INSTITUTION, ['US'])
     assert_equal(SANDBOX_INSTITUTION, response.institution.institution_id)
     assert_equal(['US'], response.institution.country_codes)
   end
 
   def test_get_by_id_invalid_parameters
     assert_raises(Plaid::InvalidInputError) do
-      client.institutions.get_by_id(BAD_STRING)
+      client.institutions.get_by_id(BAD_STRING, ['US'])
     end
   end
 
   def test_get_by_id_include_optional_metadata
     response = client.institutions.get_by_id(SANDBOX_INSTITUTION,
+                                             ['US'],
                                              options:
                                              { include_optional_metadata:
                                                true })
@@ -43,18 +49,19 @@ class PlaidInstitutionsTest < PlaidTest
   end
 
   def test_get_by_id_include_status_false
-    response = client.institutions.get_by_id(SANDBOX_INSTITUTION)
+    response = client.institutions.get_by_id(SANDBOX_INSTITUTION, ['US'])
     assert_equal(SANDBOX_INSTITUTION, response.institution.institution_id)
     assert_nil(response.institution.status)
   end
 
   def test_search
-    response = client.institutions.search(SANDBOX_INSTITUTION_NAME)
+    response = client.institutions.search(SANDBOX_INSTITUTION_NAME, ['US'])
     refute_empty(response.institutions)
   end
 
   def test_search_with_include_optional_metadata
     response = client.institutions.search SANDBOX_INSTITUTION_NAME,
+                                          ['US'],
                                           [:transactions],
                                           options:
                                           { include_optional_metadata: true }
@@ -63,19 +70,24 @@ class PlaidInstitutionsTest < PlaidTest
 
   def test_search_with_products
     response = client.institutions.search SANDBOX_INSTITUTION_NAME,
+                                          ['US'],
                                           [:transactions]
     refute_empty(response.institutions)
   end
 
   def test_search_invalid_products
     assert_raises(Plaid::InvalidInputError) do
-      client.institutions.search(SANDBOX_INSTITUTION_NAME, BAD_ARRAY)
+      client.institutions.search(SANDBOX_INSTITUTION_NAME,
+                                 ['US'],
+                                 BAD_ARRAY)
     end
   end
 
   def test_search_bad_products
     assert_raises(Plaid::InvalidInputError) do
-      client.institutions.search(SANDBOX_INSTITUTION_NAME, BAD_STRING)
+      client.institutions.search(SANDBOX_INSTITUTION_NAME,
+                                 ['US'],
+                                 BAD_STRING)
     end
   end
 end
