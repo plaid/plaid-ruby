c36c43fce9c0ac16bdb4172c3e1a2437438ee0c9
Andrew Chen <andrewchen@plaid.com>
2022-12-15 13:58:39 -0800

    OpenAPI generated code at 2022-12-15T21:58:32Z

---
+++ test/test_enrich.rb
@@ -0,0 +1,60 @@
+require_relative 'test_helper'
+
+# Internal: The test for Plaid::Enrich.
+class PlaidEnrichTest < PlaidTest
+  def test_get
+    sample_transactions_to_enrich = [
+      Plaid::ClientProvidedTransaction.new({
+        id: "1",
+        description: "DDA PURCHASE *XXXX XXXXXXXX FAMILY DOLLAR",
+        amount: 10.00,
+        direction: Plaid::EnrichTransactionDirection::OUTFLOW,
+        location: Plaid::ClientProvidedTransactionLocation.new({
+          city: "Philadelphia",
+          region: "PA",
+        }),
+        iso_currency_code: "USD",
+      }),
+      Plaid::ClientProvidedTransaction.new({
+        id: "2",
+        description: "PURCHASE JUNIATA SUPE PHILADELPHIA PA CARDXXXX",
+        amount: 10.00,
+        direction: Plaid::EnrichTransactionDirection::OUTFLOW,
+        iso_currency_code: "USD",
+      }),
+      Plaid::ClientProvidedTransaction.new({
+        id: "3",
+        description: "DEBIT CARD PURCHASE",
+        amount: 10.00,
+        direction: Plaid::EnrichTransactionDirection::OUTFLOW,
+        iso_currency_code: "USD",
+      }),
+      Plaid::ClientProvidedTransaction.new({
+        id: "4",
+        description: "DIRECT DEP* UBER.COM",
+        amount: 100.00,
+        direction: Plaid::EnrichTransactionDirection::INFLOW,
+        iso_currency_code: "USD",
+      })
+    ]
+
+    transactions_enrich_get_request = Plaid::TransactionsEnrichGetRequest.new
+
+    transactions_enrich_get_request.account_type = "depository"
+    transactions_enrich_get_request.transactions = sample_transactions_to_enrich
+
+    response = client.transactions_enrich(transactions_enrich_get_request)
+    assert_kind_of(Plaid::TransactionsEnrichGetResponse, response)
+
+    refute_empty(response.enriched_transactions)
+    assert_equal(sample_transactions_to_enrich.length, response.enriched_transactions.length)
+
+    response.enriched_transactions.each do |item|
+      assert_kind_of(Float, item.amount)
+      refute_nil(item.description)
+      refute_nil(item.direction)
+      refute_nil(item.enrichments)
+      refute_nil(item.id)
+    end
+  end
+end
